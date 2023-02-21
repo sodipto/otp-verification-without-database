84338a6e432933de10a6c1e2186545562fb4fd8c
Sodipto Saha <badhansaha444@gmail.com>
2023-02-21 10:10:51 +0600

    add base controller

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace otp_verify_without_database.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public abstract class BaseController : ControllerBase
+    {
+        protected BaseController() { }
+    }
+}
---
+++ Controllers/V1/AuthnticationController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace otp_verify_without_database.Controllers.V1
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthnticationController : BaseController
+    {
+        public AuthnticationController()
+        {
+        }
+
+    }
+}
